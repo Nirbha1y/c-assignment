a55c4712a6700156ad2a1ac40e6f7c93deed883b
Nirbha1y <102606332+Nirbha1y@users.noreply.github.com>
2022-05-19 10:32:53 +0545

    Create hcf.c

---
+++ hcf.c
@@ -0,0 +1,11 @@
+void main(int num1,int num2){
+    int hcf,i;
+    printf("Enter two numbers : ");
+    scanf("%d%d",&num1,&num2);
+    for(i=1;i<=num1 && i<=num2; i++){
+        if(num1%i==0 && num2%i==0){
+            hcf = i;
+        }
+    }
+    printf("HCF of two numbers is %d",hcf);
+}
